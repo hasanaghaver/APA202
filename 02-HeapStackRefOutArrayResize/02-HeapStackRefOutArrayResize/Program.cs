aaa7433e323f50070922d00c935273aef6b30a00
hasanagaver <hasanaa-apa202@gmail.com>
2026-03-14 22:29:39 +0400

    Task2

---
+++ 02-HeapStackRefOutArrayResize/02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task Sinifde izah etdiyim kimi yazdigimiz CustomArrResize metoduna ikinci parameter olaraq nums arrayi gondereceksiniz.
+            int[] a= { 1, 2, 3, 4};
+            int[] b= { 5, 6, 7, 8, 9 };
+            CustomArrResize(ref a,b);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+
+        public static void CustomArrResize(ref int[] arr1, int[] arr2)
+        {
+            int[] newArr = new int[arr1.Length + arr2.Length];
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i<arr1.Length)
+                {
+                    newArr[i] = arr1[i];
+                }
+                else
+                {
+                    newArr[i] = arr2[i-arr1.Length];
+                }
+            }
+            arr1 = newArr;
+
+        }
+    }
+}
