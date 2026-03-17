625c90046f0a96bfe42ab0c5ec92b4468f7cc4d5
hasanagaver <hasanaa-apa202@gmail.com>
2026-03-17 23:21:08 +0400

    Task 4

---
+++ 04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_AccessModifiresEncupsulationNamespace
+{
+    class Car : Vehicle
+    {
+        private double _fuelCapacity;
+        private double _fuel;
+        public double _fuelConsumptionPer100Km;
+
+        public double FuelCapacity
+        {
+            get { return _fuelCapacity; }
+            set { if (value > 0) { _fuelCapacity = value; } else { Console.WriteLine("Deyer musbet olmalidir!"); } }
+        }
+
+        public double Fuel
+        {
+            get { return _fuel; }
+            set { if (value > 0) { _fuel = value; } else { Console.WriteLine("Deyer musbet olmalidir!"); } }
+        }
+
+        public Car(string Brand, string Model, int Year, double fuelCapacity, double fuel, double FuelConsumptionPer100Km)
+            : base(Brand, Model, Year)
+        {
+            FuelCapacity = fuelCapacity;
+            Fuel = fuel;
+            _fuelConsumptionPer100Km = FuelConsumptionPer100Km;
+        }
+
+        public void Refuel(double liters)
+        {
+            if (_fuel + liters <= _fuelCapacity)
+            {
+                _fuel += liters;
+            }
+            else
+            {
+                Console.WriteLine("Bu qeder benzin doldura bilmezsiniz");
+            }
+        }
+    }
+}
---
+++ 04-AccessModifiresEncupsulationNamespace/04-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04_AccessModifiresEncupsulationNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new("Mercedes", "E280", 1996);
+            vehicle.StartEngine();
+            vehicle.Drive(300);
+            vehicle.StopEngine();
+            vehicle.VehicleInfo();
+
+            Console.WriteLine("----------------------------------------------");
+
+            Car car = new("BMW","F30",2013,50.0,20.0,13);
+            car.Refuel(20);
+            car.VehicleInfo();
+        }
+    }
+}
