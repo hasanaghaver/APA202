3bc8d1df2ee7bde026457f2b4e8097115b8705ea
hasanagaver <hasanaa-apa202@gmail.com>
2026-04-01 00:14:56 +0400

    Task 9

---
+++ 09-UpcastingDowncastingExplicitImplicit/Models/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_UpcastingDowncastingExplicitImplicit.Models
+{
+    internal class Dog:Animal
+    {
+        public string Breed { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 09-UpcastingDowncastingExplicitImplicit/Models/Dollar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_UpcastingDowncastingExplicitImplicit.Models
+{
+    internal class Dollar
+    {
+        public decimal USD { get; set; }
+        public Dollar(decimal usd)
+        {
+            USD = usd;
+        }
+
+        public static implicit operator Dollar(Manat manat)
+        {
+            return new Dollar(manat.AZN / 1.7m);
+        } 
+    }
+}
