cac666ecd5ac3fef0b40d2075a22a64f995e246c
hasanagaver <hasanaa-apa202@gmail.com>
2026-05-04 22:09:25 +0400

    Task 25

---
+++ 25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var student = new JsonResult(new { id = 1, name = "Ali", surname = "Quliyev" });
+
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index), "Product");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
